491d5e192f3af15b959212bf6b797ffcec8e6d3a
Stanisław Stepka <s28016@pjwstk.edu.pl>
2024-05-06 13:57:35 +0200

    Initial commit

---
+++ LinqTasks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using LinqTasks.Models;
+
+namespace LinqTasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("======= ZAD X =======");
+
+        IEnumerable<Emp> result = Tasks.Task1();
+
+        foreach (Emp emp in result)
+        {
+            Console.WriteLine(emp);
+        }
+
+        var enumerable = Tasks.Task11().ToList();
+        Console.Out.WriteLine(enumerable.Count);
+        foreach (Object o in enumerable)
+        {
+            Console.Out.WriteLine(o);
+        }
+    }
+}
---
+++ LinqTasks/Tasks.cs
@@ -0,0 +1,202 @@
+﻿using LinqTasks.Extensions;
+using LinqTasks.Models;
+
+namespace LinqTasks;
+
+public static partial class Tasks
+{
+    public static IEnumerable<Emp> Emps { get; set; }
+    public static IEnumerable<Dept> Depts { get; set; }
+
+    static Tasks()
+    {
+        Depts = LoadDepts();
+        Emps = LoadEmps();
+    }
+
+    /// <summary>
+    ///     SELECT * FROM Emps WHERE Job = "Backend programmer";
+    /// </summary>
+    public static IEnumerable<Emp> Task1()
+    {
+        return Emps
+            .Where(e => e.Job == "Backend programmer");
+    }
+
+    /// <summary>
+    ///     SELECT * FROM Emps Job = "Frontend programmer" AND Salary>1000 ORDER BY Ename DESC;
+    /// </summary>
+    public static IEnumerable<Emp> Task2()
+    {
+        return Emps
+            .Where(e => e.Job == "Frontend programmer" && e.Salary > 1000)
+            .OrderByDescending(e => e.Ename);
+    }
+
+
+    /// <summary>
+    ///     SELECT MAX(Salary) FROM Emps;
+    /// </summary>
+    public static int Task3()
+    {
+        return Emps
+            .Max(e => e.Salary);
+    }
+
+    /// <summary>
+    ///     SELECT * FROM Emps WHERE Salary=(SELECT MAX(Salary) FROM Emps);
+    /// </summary>
+    public static IEnumerable<Emp> Task4()
+    {
+        return Emps
+            .Where(e => e.Salary == Task3());
+    }
+
+    /// <summary>
+    ///    SELECT ename AS Nazwisko, job AS Praca FROM Emps;
+    /// </summary>
+    public static IEnumerable<object> Task5()
+    {
+        return Emps.Select(e => new
+        {
+            Nazwisko = e.Ename,
+            Praca = e.Job
+        });
+    }
+
+    /// <summary>
+    ///     SELECT Emps.Ename, Emps.Job, Depts.Dname FROM Emps
+    ///     INNER JOIN Depts ON Emps.Deptno=Depts.Deptno
+    ///     Rezultat: Złączenie kolekcji Emps i Depts.
+    /// </summary>
+    public static IEnumerable<object> Task6()
+    {
+        return Emps
+            .Join(Depts, e => e.Deptno, d => d.Deptno,
+                (e, d) => new
+                {
+                    e.Ename,
+                    e.Job,
+                    d.Dname
+                });
+    }
+
+    /// <summary>
+    ///     SELECT Job AS Praca, COUNT(1) LiczbaPracownikow FROM Emps GROUP BY Job;
+    /// </summary>
+    public static IEnumerable<object> Task7()
+    {
+        return Emps
+            .GroupBy(e => e.Job)
+            .Select(e => new
+            {
+                Praca = e.Key, // Key bierze z GropBy (Key -> wynik poprzedniego metoda rozszerzeń
+                LiczbaPracownikow = e.Count()
+            });
+    }
+
+    /// <summary>
+    ///     Zwróć wartość "true" jeśli choć jeden
+    ///     z elementów kolekcji pracuje jako "Backend programmer".
+    /// </summary>
+    public static bool Task8()
+    {
+        return Emps.Any(e => e.Job == "Backend programmer");
+    }
+
+    /// <summary>
+    ///     SELECT TOP 1 * FROM Emp WHERE Job="Frontend programmer"
+    ///     ORDER BY HireDate DESC;
+    /// </summary>
+    public static Emp Task9()
+    {
+        return Emps
+            .OrderByDescending(e => e.HireDate)
+            .FirstOrDefault(e => e.Job == "Frontend programmer");
+    }
+
+    /// <summary>
+    ///     SELECT Ename, Job, Hiredate FROM Emps
+    ///     UNION
+    ///     SELECT "Brak wartości", null, null;
+    /// </summary>
+    public static IEnumerable<object> Task10()
+    {
+        var emp = Emps
+            .Select(e => new
+            {
+                Ename = e.Ename,
+                Job = e.Job,
+                HireDate = e.HireDate
+            });
+
+        var def = new[]
+        {
+            new
+            {
+                Ename = "Brak wartości",
+                Job = (string)null,
+                HireDate = (DateTime?)null
+            }
+        };
+
+        return emp.Union(def);
+    }
+
+    /// <summary>
+    ///     Wykorzystując LINQ pobierz pracowników podzielony na departamenty pamiętając, że:
+    ///     1. Interesują nas tylko departamenty z liczbą pracowników powyżej 1
+    ///     2. Chcemy zwrócić listę obiektów o następującej strukturze:
+    ///     [
+    ///     {name: "RESEARCH", numOfEmployees: 3},
+    ///     {name: "SALES", numOfEmployees: 5},
+    ///     ...
+    ///     ]
+    ///     3. Wykorzystaj typy anonimowe
+    /// </summary>
+    public static IEnumerable<object> Task11()
+    {
+        return Depts
+            .GroupBy(d => d.Dname)
+            .Where(d => d.Count() > 1)
+            .Select(d => new
+            {
+                Name = d.Key,
+                NumOfEmployees = d.Count()
+            });
+    }
+
+    /// <summary>
+    ///     Napisz własną metodę rozszerzeń, która pozwoli skompilować się poniższemu fragmentowi kodu.
+    ///     Metodę dodaj do klasy CustomExtensionMethods, która zdefiniowana jest poniżej.
+    ///     Metoda powinna zwrócić tylko tych pracowników, którzy mają min. 1 bezpośredniego podwładnego.
+    ///     Pracownicy powinny w ramach kolekcji być posortowani po nazwisku (rosnąco) i pensji (malejąco).
+    /// </summary>
+    public static IEnumerable<Emp> Task12()
+    {
+        IEnumerable<Emp> result = Emps.GetEmpsWithSubordinates();
+
+        return result;
+    }
+
+    /// <summary>
+    ///     Poniższa metoda powinna zwracać pojedyczną liczbę int.
+    ///     Na wejściu przyjmujemy listę liczb całkowitych.
+    ///     Spróbuj z pomocą LINQ'a odnaleźć tę liczbę, które występuja w tablicy int'ów nieparzystą liczbę razy.
+    ///     Zakładamy, że zawsze będzie jedna taka liczba.
+    ///     Np: {1,1,1,1,1,1,10,1,1,1,1} => 10
+    /// </summary>
+    public static int Task13(int[] arr)
+    {
+        return -1;
+    }
+
+    /// <summary>
+    ///     Zwróć tylko te departamenty, które mają 5 pracowników lub nie mają pracowników w ogóle.
+    ///     Posortuj rezultat po nazwie departament rosnąco.
+    /// </summary>
+    public static IEnumerable<Dept> Task14()
+    {
+        return null;
+    }
+}
